b08c539d141568f61b03e45f3f6272b691171da5
HIGAKI Masaru <mas.higa@gmail.com>
2019-07-08 19:22:34 +0900

    Step 6: Green

--- test_stack.rb
+++ test_stack.rb
@@ -33,14 +33,24 @@ class TestStack < Test::Unit::TestCase
     assert_equal 1, @stack.size
     refute @stack.empty?
   end
+
+  def test_pop_by_1_element
+    @stack.push "Alice"
+    assert_equal 1, @stack.size
+    refute @stack.empty?
+
+    assert_equal "Alice", @stack.pop
+    assert_equal 0, @stack.size
+    assert @stack.empty?
+  end
 end
 
 # >> Loaded suite -
 # >> Started
-# >> .....
-# >> Finished in 0.000612 seconds.
+# >> ......
+# >> Finished in 0.000883 seconds.
 # >> -------------------------------------------------------------------------------
-# >> 5 tests, 9 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 6 tests, 14 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
 # >> 100% passed
 # >> -------------------------------------------------------------------------------
-# >> 8169.93 tests/s, 14705.88 assertions/s
+# >> 6795.02 tests/s, 15855.04 assertions/s
